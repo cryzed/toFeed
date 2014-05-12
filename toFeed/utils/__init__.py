20e0e18fa0e93bf8f8479635bb06e9873a6db573
cryzed <cryzed@googlemail.com>
2014-05-12 18:58:55 +0200

    Fix directory structure 1

--- toFeed/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-def shorten_to_title(title, length, separator=' ', appendix='...'):
-    """
-    Tries to split the given title in a human readable way. It searches for the
-    last occurrence of a space within the title limited by the given length and
-    splits it there.
-    """
-    return title[:length].rpartition(separator)[0] + appendix
