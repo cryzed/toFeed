a256b360de2d97fb63c5c8abeb552d52398a9020
cryzed <cryzed@googlemail.com>
2014-05-14 21:35:12 +0200

    Renamed parsing to scraping

--- tofeed/adapters/patreon.py
+++ tofeed/adapters/patreon.py
@@ -24,48 +24,48 @@ class ActivityFeed(tofeed.adapters.Adapter):
         self.max_title_length = int(max_title_length)
 
     @staticmethod
-    def _parse_content(element):
+    def _scrape_content(element):
         share_content = element.find('div', {'class': 'shareContent'})
         spoon.convert_newlines(share_content)
         return share_content
 
     @staticmethod
-    def _parse_author(element):
+    def _scrape_author(element):
         author = ''.join(element.find('p', {'class': 'info'}).stripped_strings)
         return author
 
-    def _parse_date(self, element):
+    def _scrape_date(self, element):
         date_string = element.find('div', {'class': 'dateBox'}).string.strip()
         date = datetime.strptime(date_string, self.DATE_FORMAT)
         return date
 
-    def _parse_image(self, element):
+    def _scrape_image(self, element):
         image_popup = element.find('a', {'class': 'imagePopup'})
         spoon.absolutize_references(self.url, image_popup, recursive=False)
         return image_popup
 
-    def _parse_link(self, element):
+    def _scrape_link(self, element):
         anchor = element.find('a', rel='shares')
         spoon.absolutize_references(self.url, anchor, recursive=False)
         return anchor['href']
 
-    def _parse_note(self, note_soup):
-        author = self._parse_author(note_soup)
-        content = self._parse_content(note_soup)
-        date = self._parse_date(note_soup)
-        link = self._parse_link(note_soup)
+    def _scrape_note(self, note_soup):
+        author = self._scrape_author(note_soup)
+        content = self._scrape_content(note_soup)
+        date = self._scrape_date(note_soup)
+        link = self._scrape_link(note_soup)
         return None, link, (content,), author, date
 
-    def _parse_photo(self, photo_soup):
-        author = self._parse_author(photo_soup)
-        content = self._parse_content(photo_soup)
-        image = self._parse_image(photo_soup)
-        date = self._parse_date(photo_soup)
-        link = self._parse_link(photo_soup)
+    def _scrape_photo(self, photo_soup):
+        author = self._scrape_author(photo_soup)
+        content = self._scrape_content(photo_soup)
+        image = self._scrape_image(photo_soup)
+        date = self._scrape_date(photo_soup)
+        link = self._scrape_link(photo_soup)
         return None, link, (image, content), author, date
 
-    def _parse_mylink(self, mylink_soup):
-        author = self._parse_author(mylink_soup)
+    def _scrape_mylink(self, mylink_soup):
+        author = self._scrape_author(mylink_soup)
 
         title_anchor = mylink_soup.find('p', {'class': 'title'}).a
         title = title_anchor.string.strip()
@@ -73,24 +73,24 @@ class ActivityFeed(tofeed.adapters.Adapter):
         spoon.absolutize_references(self.url, title_anchor, recursive=False)
         link = title_anchor['href']
 
-        image = self._parse_image(mylink_soup)
+        image = self._scrape_image(mylink_soup)
 
         # Apparently newlines in this section are not even included, so there's
         # no need to convert newlines
         link_description = mylink_soup.find('p', {'class': 'linkDesc'})
-        content = self._parse_content(mylink_soup)
-        date = self._parse_date(mylink_soup)
+        content = self._scrape_content(mylink_soup)
+        date = self._scrape_date(mylink_soup)
         return title, link, (image, link_description, content), author, date
 
-    def _parse_activity(self, activity_soup):
+    def _scrape_activity(self, activity_soup):
         if 'note' in activity_soup['class']:
-            return self._parse_note(activity_soup)
+            return self._scrape_note(activity_soup)
 
         if 'photo' in activity_soup['class']:
-            return self._parse_photo(activity_soup)
+            return self._scrape_photo(activity_soup)
 
         if 'mylink' in activity_soup['class']:
-            return self._parse_mylink(activity_soup)
+            return self._scrape_mylink(activity_soup)
 
     def to_feed(self):
         response = urllib2.urlopen(self.url)
@@ -108,7 +108,7 @@ class ActivityFeed(tofeed.adapters.Adapter):
             if prv is None or prv in ['1', '100']:
                 continue
 
-            title, link, contents, author, date = self._parse_activity(activity)
+            title, link, contents, author, date = self._scrape_activity(activity)
             main_element = contents[-1]
             if 'note' in activity['class'] or 'photo' in activity['class']:
                 content_string = ' '.join(main_element.stripped_strings)
