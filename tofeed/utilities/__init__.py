420c7bee4b420e08d4a621cb9e2d08dfc8250e7b
cryzed <cryzed@googlemail.com>
2014-05-14 18:13:42 +0200

    Clarify docstrings

--- tofeed/utilities/__init__.py
+++ tofeed/utilities/__init__.py
@@ -1,13 +1,15 @@
 def shorten_to_title(string, length, separator=' ', appendix='...'):
     """
-    Shortens the string to a human readable title. The function searches for
-    the last occurrence of separator within the range of the content limited by
-    length and appends the appendix.
+    Use the string to create a human readable title.
+
+    The function searches for the last occurrence of the separator string within
+    the range of the string limited by the length parameter and appends the
+    appendix.
 
     :param str string: The string to create the title out of
     :param int length: The approximate length of the title. The length is only
         approximate to this value, because it's possible that the next separator
-        encountered may lie further ahead.
+        character encountered may lie further ahead.
     :param str separator: The character that separates words in the text.
         Usually of course a space, this should only have to be changed in very
         rare cases.
