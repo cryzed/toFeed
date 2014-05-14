7b69db7961b7752594a7565ded30493e273f7bac
cryzed <cryzed@googlemail.com>
2014-05-14 10:21:05 +0200

    Took care of a TODO and improved the Patreon Adapter

--- tofeed/adapters/patreon.py
+++ tofeed/adapters/patreon.py
@@ -53,7 +53,7 @@ class ActivityFeed(tofeed.adapters.Adapter):
         content = self._parse_content(note_soup)
         date = self._parse_date(note_soup)
         link = self._parse_link(note_soup)
-        return link, content, author, date
+        return None, link, (content,), author, date
 
     def _parse_photo(self, photo_soup):
         author = self._parse_author(photo_soup)
@@ -61,7 +61,7 @@ class ActivityFeed(tofeed.adapters.Adapter):
         image = self._parse_image(photo_soup)
         date = self._parse_date(photo_soup)
         link = self._parse_link(photo_soup)
-        return link, content, image, author, date
+        return None, link, (image, content), author, date
 
     def _parse_mylink(self, mylink_soup):
         author = self._parse_author(mylink_soup)
@@ -79,7 +79,17 @@ class ActivityFeed(tofeed.adapters.Adapter):
         link_description = mylink_soup.find('p', {'class': 'linkDesc'})
         content = self._parse_content(mylink_soup)
         date = self._parse_date(mylink_soup)
-        return title, link, image, link_description, content, author, date
+        return title, link, (image, link_description, content), author, date
+
+    def _parse_activity(self, activity_soup):
+        if 'note' in activity_soup['class']:
+            return self._parse_note(activity_soup)
+
+        if 'photo' in activity_soup['class']:
+            return self._parse_photo(activity_soup)
+
+        if 'mylink' in activity_soup['class']:
+            return self._parse_mylink(activity_soup)
 
     def to_feed(self):
         response = urllib2.urlopen(self.url)
@@ -88,46 +98,29 @@ class ActivityFeed(tofeed.adapters.Adapter):
         title = soup.title.string
         now = datetime.now()
         feed = tofeed.formats.rss.Channel(title, self.url, title, pub_date=now, last_build_date=now)
+
+        # Don't recursively search for all divs, we only want the ones one level
+        # lower.
         for activity in soup.find('div', id='boxGrid')('div', recursive=False):
             # Ignore for patreon members only activities
             prv = activity.get('prv')
             if prv is None or prv in ['1', '100']:
                 continue
 
-            if 'note' in activity['class']:
-                link, content, author, date = self._parse_note(activity)
-
-                # TODO: Possibly stop modifying the soup in the parsing methods
-                # and do it here, this way I wouldn't need to grab the content
-                # string this way again.
-                content_string = ' '.join(content.stripped_strings)
-
+            title, link, contents, author, date = self._parse_activity(activity)
+            main_element = contents[-1]
+            if any((klass in activity['class'] for klass in ('note', 'photo'))):
+                content_string = ' '.join(main_element.stripped_strings)
                 if self.max_title_length >= len(content_string):
                     title = content_string
                 else:
                     title = tofeed.utilities.shorten_to_title(content_string, self.max_title_length)
 
-                feed.add(title, link, unicode(content), author=author, pub_date=date)
+            for index, content in enumerate(contents):
+                # When the root element is encountered stop.
+                if index == len(contents) - 1:
+                    break
+                main_element.insert(index, content)
 
-            elif 'photo' in activity['class']:
-                link, content, image, author, date = self._parse_photo(activity)
-
-                # TODO: Possibly stop modifying the soup in the parsing methods
-                # and do it here, this way I wouldn't need to grab the content
-                # string this way again.
-                content_string = ' '.join(content.stripped_strings)
-
-                if self.max_title_length >= len(content_string):
-                    title = content_string
-                else:
-                    title = tofeed.utilities.shorten_to_title(content_string, self.max_title_length)
-
-                content.insert(0, image)
-                feed.add(title, link, unicode(content), author=author, pub_date=date)
-
-            elif 'mylink' in activity['class']:
-                title, link, image, link_description, content, author, date = self._parse_mylink(activity)
-                content.insert(0, link_description)
-                content.insert(0, image)
-                feed.add(title, link, unicode(content), author=author, pub_date=date)
+            feed.add(title, link, unicode(content), author=author, pub_date=date)
         return feed.generate()
